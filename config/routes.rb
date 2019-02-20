db7416d184b06b53795611320fa07d0c75b127fe
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-02-20 15:37:21 +0000

    need some editing in the style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :restaurants do
+      resources :reviews
+    end
+  end
+
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'french',
+    phone_number: '2183223923'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '03920930293'
+
+  },
+  {
+    name:         'Pizza West',
+    address:      '32A Shoreditch Low St, London E2 9PQ',
+    category:     'chinese',
+    phone_number: '83019213'
+  },
+  {
+    name:         'Pizza North',
+    address:      '92A Shoreditch Low St, Lisbon E2 1PQ',
+    category:     'japanese',
+    phone_number:  '74303232'
+  },
+  {
+    name:         'Pizza Express',
+    address:      '92A Shoreditch Low St, Lisbon E2 1PQ',
+    category:     'japanese',
+    phone_number:  '27803232'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
